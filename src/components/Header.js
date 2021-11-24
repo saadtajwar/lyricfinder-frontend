34426312ae70c6d9696f8db0eb8fc99351d3e822
Saad Tajwar <saads@ad.unc.edu>
2021-11-24 01:22:08 -0500

    Header display

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,30 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {Link, useLocation} from 'react-router-dom'
 
 const Header = () => {
+    const location = useLocation().pathname;
+    const [displayOrNot, setDisplayOrNot] = useState(true);
+
+    const visible = { display: displayOrNot ? "" : "none"};
+    console.log(displayOrNot);
+
+    useEffect(()=> {
+        const setDisplaySearch = () => {
+            if (location === '/search') {
+                setDisplayOrNot(false);
+            } else {
+                setDisplayOrNot(true);
+            }
+        }
+        setDisplaySearch();
+    }, [location])
+    
     return (
         <div>
             <div className="box">
                 <Link to='/' className="title">LyricFinder</Link>
             </div>
-            <div className="box">
+            <div className="box" style={visible}>
                 <Link to='/search' className="title">Search for Tracks by Artist or Song Title</Link>
             </div>
             <div className="box">
